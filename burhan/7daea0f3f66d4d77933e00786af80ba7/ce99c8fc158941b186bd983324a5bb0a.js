445e98e341264e2c662fbc08d4eabb0202d0791b
Sachin Sinha <sachin@iqlect.com>
2023-04-05 19:37:56 +0530

    Update file

--- burhan/7daea0f3f66d4d77933e00786af80ba7/ce99c8fc158941b186bd983324a5bb0a.js
+++ burhan/7daea0f3f66d4d77933e00786af80ba7/ce99c8fc158941b186bd983324a5bb0a.js
@@ -6499,7 +6499,63 @@
       });
     }
   };
-})();
+})();Templates["AllDownloadsClickTrigger"] = (function () {
+    return function (parameters, TagManager) {
+      this.setUp = function (triggerEvent) {
+        TagManager.dom.onReady(function () {
+          var extensions = parameters.get("downloadExtensions");
+          if (!extensions) {
+            return;
+          }
+          function isClickNode(nodeName) {
+            return nodeName === "A" || nodeName === "AREA";
+          }
+          TagManager.dom.onClick(function (event, clickButton) {
+            clickCallback(event, triggerEvent, clickButton);
+          });
+          function clickCallback(event, triggerEvent, clickButton) {
+            if (!event.target) {
+              return;
+            }
+            var target = event.target;
+            if (target.shadowRoot) {
+              var composedPath = event.composedPath();
+              if (composedPath.length) {
+                target = composedPath[0];
+              }
+            }
+            var nodeName = target.nodeName;
+            while (!isClickNode(nodeName) && target && target.parentNode) {
+              target = target.parentNode;
+              nodeName = target.nodeName;
+            }
+            extensions = String(extensions).split(",");
+            var i;
+            for (i = 0; i < extensions.length; i++) {
+              extensions[i] = TagManager.utils.trim(extensions[i]);
+            }
+            if (target && isClickNode(nodeName)) {
+              var link = TagManager.dom.getElementAttribute(target, "href");
+              var downloadExtensionsPattern = new RegExp("\\.(" + extensions.join("|") + ")([?&#]|$)", "i");
+              if (downloadExtensionsPattern.test(link)) {
+                triggerEvent({
+                  event: "mtm.DownloadClick",
+                  "mtm.clickElement": target,
+                  "mtm.clickElementId": TagManager.dom.getElementAttribute(target, "id"),
+                  "mtm.clickElementClasses": TagManager.dom.getElementClassNames(target),
+                  "mtm.clickText": TagManager.dom.getElementText(target),
+                  "mtm.clickNodeName": nodeName,
+                  "mtm.clickElementUrl": link,
+                  "mtm.clickButton": clickButton,
+                });
+              }
+            }
+          }
+        });
+      };
+    };
+  })();
+
       //search here
       
       Templates["ClickClassesVariable"] = (function () {
@@ -6641,6 +6697,77 @@
   "Type": "BangDB Analytics",
   "blockedTriggerIds": []
 },
+        {
+  "name": "9134dd9d2194bc388b19a90c09d1efc1",
+  "Type": "BangDB Analytics",
+  "id": "38f277da-c2b3-4015-8d7e-c6d8b57c343b",
+  "type": "Matomo",
+  "parameters": {
+    "matomoConfig": {
+      "name": "Matomo Configuration",
+      "type": "MatomoConfiguration",
+      "lookUpTable": [],
+      "defaultValue": "",
+      "parameters": {
+        "matomoUrl": "https://testbe.bangdb.com:18080",
+        "idSite": "1",
+        "enableLinkTracking": true,
+        "enableCrossDomainLinking": true,
+        "enableDoNotTrack": false,
+        "enableJSErrorTracking": true,
+        "enableHeartBeatTimer": true,
+        "trackAllContentImpressions": true,
+        "trackVisibleContentImpressions": true,
+        "disableCookies": false,
+        "requireConsent": false,
+        "requireCookieConsent": false,
+        "customCookieTimeOutEnable": false,
+        "customCookieTimeOut": 393,
+        "setSecureCookie": true,
+        "cookieDomain": "",
+        "cookiePath": "",
+        "cookieSameSite": "Lax",
+        "disableBrowserFeatureDetection": false,
+        "domains": [],
+        "alwaysUseSendBeacon": false,
+        "userId": "",
+        "customDimensions": [],
+        "bundleTracker": true,
+        "registerAsDefaultTracker": true,
+        "jsEndpoint": "matomo.js",
+        "trackingEndpoint": "stream/ShopIQ/VisitorData"
+      },
+      "Variable": "MatomoConfigurationVariable"
+    },
+    "trackingType": "event",
+    "idGoal": "",
+    "goalCustomRevenue": "",
+    "documentTitle": "Download_Trigger_test1",
+    "customUrl": "",
+    "eventCategory": "Download_Trigger_test1",
+    "eventAction": "Download_Trigger_test1",
+    "eventName": "Download_Trigger_test1",
+    "eventValue": {
+      "name": "ClickClasses",
+      "type": "ClickClasses",
+      "lookUpTable": [],
+      "defaultValue": null,
+      "parameters": [],
+      "Variable": "ClickClassesVariable"
+    },
+    "selectedTag": "BangDB Analytics",
+    "Name": "Download_Trigger_test1",
+    "Description": "Download_Trigger_test1"
+  },
+  "blockTriggerIds": [],
+  "fireTriggerIds": [],
+  "fireLimit": "unlimited",
+  "fireDelay": 0,
+  "startDate": null,
+  "endDate": null,
+  "Tag": "MatomoTag",
+  "blockedTriggerIds": []
+},
           ],
           triggers: [
             
@@ -6654,6 +6781,19 @@
   "conditions": [],
   "Name": "All elements click trigger",
   "Description": "description"
+}
+          {
+  "id": "dad40650-2749-41ab-9ba4-0325a734c143",
+  "type": "AllDownloadsClick",
+  "name": "AllDownloadsClick",
+  "Trigger": "AllDownloadsClickTrigger",
+  "selectedTrigger": "All Downloads Click",
+  "parameters": {
+    "downloadExtensions": "Downloading_trigger"
+  },
+  "conditions": [],
+  "Name": "Download_Trigger_test1",
+  "Description": "testing downloading trigger"
 }
           ],
           variables: [
