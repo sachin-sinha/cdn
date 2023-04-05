20cfa248d70efe21cda75dad4cdbb661850225f8
Sachin Sinha <sachin@iqlect.com>
2023-04-05 23:52:47 +0530

    Update file

--- burhan/eb6859b4b16b460594937e3579d7e1ca/eac9a8ed3a8a4445a63bbc8542021547.js
+++ burhan/eb6859b4b16b460594937e3579d7e1ca/eac9a8ed3a8a4445a63bbc8542021547.js
@@ -6499,6 +6499,48 @@
       });
     }
   };
+})();Templates["AllLinksClickTrigger"] = (function () {
+  return function (parameters, TagManager) {
+    this.setUp = function (triggerEvent) {
+      TagManager.dom.onReady(function () {
+        function isClickNode(nodeName) {
+          return nodeName === "A" || nodeName === "AREA";
+        }
+        TagManager.dom.onClick(function (event, clickButton) {
+          clickCallback(event, triggerEvent, clickButton);
+        });
+        function clickCallback(event, triggerEvent, clickButton) {
+          if (!event.target) {
+            return;
+          }
+          var target = event.target;
+          if (target.shadowRoot) {
+            var composedPath = event.composedPath();
+            if (composedPath.length) {
+              target = composedPath[0];
+            }
+          }
+          var nodeName = target.nodeName;
+          while (!isClickNode(nodeName) && target && target.parentNode) {
+            target = target.parentNode;
+            nodeName = target.nodeName;
+          }
+          if (target && isClickNode(nodeName)) {
+            triggerEvent({
+              event: "mtm.AllLinksClick",
+              "mtm.clickElement": target,
+              "mtm.clickElementId": TagManager.dom.getElementAttribute(target, "id"),
+              "mtm.clickElementClasses": TagManager.dom.getElementClassNames(target),
+              "mtm.clickText": TagManager.dom.getElementText(target),
+              "mtm.clickNodeName": nodeName,
+              "mtm.clickElementUrl": TagManager.dom.getElementAttribute(target, "href"),
+              "mtm.clickButton": clickButton,
+            });
+          }
+        }
+      });
+    };
+  };
 })();
       //search here
       
@@ -6641,6 +6683,79 @@
   "Type": "BangDB Analytics",
   "blockedTriggerIds": []
 },
+        {
+  "name": "9134dd9d2194bc388b19a90c09d1efc1",
+  "Type": "BangDB Analytics",
+  "id": "44da7963-16e0-4a45-962a-9029c32f2b7b",
+  "type": "Matomo",
+  "parameters": {
+    "matomoConfig": {
+      "name": "Matomo Configuration",
+      "type": "MatomoConfiguration",
+      "lookUpTable": [],
+      "defaultValue": "",
+      "parameters": {
+        "matomoUrl": "https://testbe.bangdb.com:18080",
+        "idSite": "1",
+        "enableLinkTracking": true,
+        "enableCrossDomainLinking": true,
+        "enableDoNotTrack": false,
+        "enableJSErrorTracking": true,
+        "enableHeartBeatTimer": true,
+        "trackAllContentImpressions": true,
+        "trackVisibleContentImpressions": true,
+        "disableCookies": false,
+        "requireConsent": false,
+        "requireCookieConsent": false,
+        "customCookieTimeOutEnable": false,
+        "customCookieTimeOut": 393,
+        "setSecureCookie": true,
+        "cookieDomain": "",
+        "cookiePath": "",
+        "cookieSameSite": "Lax",
+        "disableBrowserFeatureDetection": false,
+        "domains": [],
+        "alwaysUseSendBeacon": false,
+        "userId": "",
+        "customDimensions": [],
+        "bundleTracker": true,
+        "registerAsDefaultTracker": true,
+        "jsEndpoint": "matomo.js",
+        "trackingEndpoint": "stream/ShopIQ/VisitorData"
+      },
+      "Variable": "MatomoConfigurationVariable"
+    },
+    "trackingType": "event",
+    "idGoal": "",
+    "goalCustomRevenue": "",
+    "documentTitle": "",
+    "customUrl": "",
+    "eventCategory": "Link_Trigger",
+    "eventAction": "Link_Trigger",
+    "eventName": "Link_Trigger_ClickClasses",
+    "eventValue": {
+      "name": "ClickClasses",
+      "type": "ClickClasses",
+      "lookUpTable": [],
+      "defaultValue": null,
+      "parameters": [],
+      "Variable": "ClickClassesVariable"
+    },
+    "selectedTag": "BangDB Analytics",
+    "Name": "Link_Trigger_ClickClasses",
+    "Description": "Link_Trigger test for ClickClasses"
+  },
+  "blockTriggerIds": [],
+  "fireTriggerIds": [
+    "2a728c42-fdc2-4ab1-92eb-6a05d03b9ef4"
+  ],
+  "fireLimit": "unlimited",
+  "fireDelay": 0,
+  "startDate": null,
+  "endDate": null,
+  "Tag": "MatomoTag",
+  "blockedTriggerIds": []
+},
           ],
           triggers: [
             
@@ -6654,6 +6769,17 @@
   "conditions": [],
   "Name": "All_Elements_Click",
   "Description": "Testing Element click"
+}
+          {
+  "id": "2a728c42-fdc2-4ab1-92eb-6a05d03b9ef4",
+  "type": "AllLinksClick",
+  "name": "AllLinksClick",
+  "Trigger": "AllLinksClickTrigger",
+  "selectedTrigger": "All Links Click",
+  "parameters": {},
+  "conditions": [],
+  "Name": "Link_Trigger",
+  "Description": "Link_Trigger"
 }
           ],
           variables: [
