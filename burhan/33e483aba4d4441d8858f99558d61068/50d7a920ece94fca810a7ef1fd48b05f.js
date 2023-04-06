11bdb1020d70e3ebab2f90dac4fba2eed1119e04
Sachin Sinha <sachin@iqlect.com>
2023-04-06 13:37:47 +0530

    Update file

--- burhan/33e483aba4d4441d8858f99558d61068/50d7a920ece94fca810a7ef1fd48b05f.js
+++ burhan/33e483aba4d4441d8858f99558d61068/50d7a920ece94fca810a7ef1fd48b05f.js
@@ -6541,7 +6541,63 @@
       });
     };
   };
-})();
+})();Templates["AllDownloadsClickTrigger"] = (function () {
+    return function (parameters, TagManager) {
+      this.setUp = function (triggerEvent) {
+        TagManager.dom.onReady(function () {
+          var extensions = parameters.get("downloadExtensions");
+          if (!extensions) {
+            return;
+          }
+          function isClickNode(nodeName) {
+            return nodeName === "A" || nodeName === "AREA";
+          }
+          TagManager.dom.onClick(function (event, clickButton) {
+            clickCallback(event, triggerEvent, clickButton);
+          });
+          function clickCallback(event, triggerEvent, clickButton) {
+            if (!event.target) {
+              return;
+            }
+            var target = event.target;
+            if (target.shadowRoot) {
+              var composedPath = event.composedPath();
+              if (composedPath.length) {
+                target = composedPath[0];
+              }
+            }
+            var nodeName = target.nodeName;
+            while (!isClickNode(nodeName) && target && target.parentNode) {
+              target = target.parentNode;
+              nodeName = target.nodeName;
+            }
+            extensions = String(extensions).split(",");
+            var i;
+            for (i = 0; i < extensions.length; i++) {
+              extensions[i] = TagManager.utils.trim(extensions[i]);
+            }
+            if (target && isClickNode(nodeName)) {
+              var link = TagManager.dom.getElementAttribute(target, "href");
+              var downloadExtensionsPattern = new RegExp("\\.(" + extensions.join("|") + ")([?&#]|$)", "i");
+              if (downloadExtensionsPattern.test(link)) {
+                triggerEvent({
+                  event: "mtm.DownloadClick",
+                  "mtm.clickElement": target,
+                  "mtm.clickElementId": TagManager.dom.getElementAttribute(target, "id"),
+                  "mtm.clickElementClasses": TagManager.dom.getElementClassNames(target),
+                  "mtm.clickText": TagManager.dom.getElementText(target),
+                  "mtm.clickNodeName": nodeName,
+                  "mtm.clickElementUrl": link,
+                  "mtm.clickButton": clickButton,
+                });
+              }
+            }
+          }
+        });
+      };
+    };
+  })();
+
       //search here
       
       Templates["ClickClassesVariable"] = (function () {
@@ -6853,6 +6909,19 @@
   "conditions": [],
   "Name": "Link_Trigger",
   "Description": "testing Link trigger"
+}
+          {
+  "id": "58db6ee2-5418-4a46-8b40-5ea15bd3e00d",
+  "type": "AllDownloadsClick",
+  "name": "AllDownloadsClick",
+  "Trigger": "AllDownloadsClickTrigger",
+  "selectedTrigger": "All Downloads Click",
+  "parameters": {
+    "downloadExtensions": "csv,jpeg"
+  },
+  "conditions": [],
+  "Name": "Download_Trigger",
+  "Description": "Download_Trigger_test1"
 }
           ],
           variables: [
