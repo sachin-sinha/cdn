490036af7646c2d1994d6633991cf48eae872faa
Sachin Sinha <sachin@iqlect.com>
2023-04-06 15:00:53 +0530

    Update file

--- matomoTestUser/4deafbcd307941eab6f4bf0b34ae18ff/mastercontainer.js
+++ matomoTestUser/4deafbcd307941eab6f4bf0b34ae18ff/mastercontainer.js
@@ -3,7 +3,7 @@
     var d = document,
     s = d.getElementsByTagName('script')[0];
     containerList.forEach(container => {
-      fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/burhan/4deafbcd307941eab6f4bf0b34ae18ff/${container}.js`)
+      fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/matomoTestUser/4deafbcd307941eab6f4bf0b34ae18ff/${container}.js`)
       .then(response => response.text())
       .then(scriptText => {
         const script = document.createElement('script');
