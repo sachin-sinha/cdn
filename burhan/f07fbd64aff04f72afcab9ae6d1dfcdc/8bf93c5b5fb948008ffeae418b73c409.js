f5ec0385ebb268dbb4584a5fddf300aeed50143f
Sachin Sinha <sachin@iqlect.com>
2023-04-07 13:57:26 +0530

    Update file

--- burhan/f07fbd64aff04f72afcab9ae6d1dfcdc/8bf93c5b5fb948008ffeae418b73c409.js
+++ burhan/f07fbd64aff04f72afcab9ae6d1dfcdc/8bf93c5b5fb948008ffeae418b73c409.js
@@ -6467,40 +6467,7 @@
           };
         };
       })();
-      Templates["FormSubmitTrigger"] = (function () {
-  return function (parameters, TagManager) {
-    this.setUp = function (triggerEvent) {
-      TagManager.dom.onReady(function () {
-        TagManager.dom.addEventListener(
-          parameters.document.body,
-          "submit",
-          function (event) {
-            if (!event.target) {
-              return;
-            }
-            var target = event.target;
-            if (target.nodeName === "FORM") {
-              var dom = TagManager.dom;
-              var formAction = dom.getElementAttribute(target, "action");
-              if (!formAction) {
-                formAction = parameters.window.location.href;
-              }
-              triggerEvent({
-                event: "mtm.FormSubmit",
-                "mtm.formElement": target,
-                "mtm.formElementId": dom.getElementAttribute(target, "id"),
-                "mtm.formElementName": dom.getElementAttribute(target, "name"),
-                "mtm.formElementClasses": dom.getElementClassNames(target),
-                "mtm.formElementAction": formAction,
-              });
-            }
-          },
-          true
-        );
-      });
-    };
-  };
-})();
+      
       //search here
       
       Templates["ClickClassesVariable"] = (function () {
@@ -6569,99 +6536,9 @@
           environment: "live",
           tags: [
             
-        {
-  "name": "9134dd9d2194bc388b19a90c09d1efc1",
-  "Type": "BangDB Analytics",
-  "id": "5aa55df1-58d5-4dff-b93f-ee6b3970170a",
-  "type": "Matomo",
-  "parameters": {
-    "matomoConfig": {
-      "name": "Matomo Configuration",
-      "type": "MatomoConfiguration",
-      "lookUpTable": [],
-      "defaultValue": "",
-      "parameters": {
-        "matomoUrl": "https://testbe.bangdb.com:18080",
-        "idSite": "1",
-        "enableLinkTracking": true,
-        "enableCrossDomainLinking": true,
-        "enableDoNotTrack": false,
-        "enableJSErrorTracking": true,
-        "enableHeartBeatTimer": true,
-        "trackAllContentImpressions": true,
-        "trackVisibleContentImpressions": true,
-        "disableCookies": false,
-        "requireConsent": false,
-        "requireCookieConsent": false,
-        "customCookieTimeOutEnable": false,
-        "customCookieTimeOut": 393,
-        "setSecureCookie": true,
-        "cookieDomain": "",
-        "cookiePath": "",
-        "cookieSameSite": "Lax",
-        "disableBrowserFeatureDetection": false,
-        "domains": [],
-        "alwaysUseSendBeacon": false,
-        "userId": "",
-        "customDimensions": [],
-        "bundleTracker": true,
-        "registerAsDefaultTracker": true,
-        "jsEndpoint": "matomo.js",
-        "trackingEndpoint": "stream/ShopIQ/VisitorData"
-      },
-      "Variable": "MatomoConfigurationVariable"
-    },
-    "trackingType": "event",
-    "idGoal": "",
-    "goalCustomRevenue": "",
-    "documentTitle": "",
-    "customUrl": "",
-    "eventCategory": "Form submit",
-    "eventAction": "Form submit",
-    "eventName": "Form submit",
-    "eventValue": {
-      "name": "FormElement",
-      "type": "FormElement",
-      "lookUpTable": [],
-      "defaultValue": null,
-      "parameters": [],
-      "Variable": "FormElementVariable"
-    },
-    "selectedTag": "BangDB Analytics",
-    "Name": "Form submit",
-    "Description": "Form submit"
-  },
-  "blockTriggerIds": [],
-  "fireTriggerIds": [
-    "f82e27cc-4778-4464-9e8b-6945440f794a"
-  ],
-  "fireLimit": "unlimited",
-  "fireDelay": 0,
-  "startDate": null,
-  "endDate": null,
-  "Tag": "MatomoTag",
-  "blockedTriggerIds": []
-},
           ],
           triggers: [
             
-          {
-  "id": "f82e27cc-4778-4464-9e8b-6945440f794a",
-  "type": "FormSubmit",
-  "name": "FormSubmit",
-  "Trigger": "FormSubmitTrigger",
-  "selectedTrigger": "Form Submit",
-  "parameters": {},
-  "conditions": [
-    {
-      "event_type": "Form Element",
-      "operation": "equals",
-      "value": "[object HTMLFormElement]"
-    }
-  ],
-  "Name": "Form Submit",
-  "Description": "Form Submit"
-},
           ],
           variables: [
             
