76826a70c3156cf6bba1b04c70f14812b9a572b0
Sachin Sinha <sachin@iqlect.com>
2023-04-07 19:44:20 +0530

    Update file

--- NewPowerUser/9c9b47b6fe964f0c8d4864aeb011fb6b/25b32f66402c40e7913a5034a592e895.js
+++ NewPowerUser/9c9b47b6fe964f0c8d4864aeb011fb6b/25b32f66402c40e7913a5034a592e895.js
@@ -6507,7 +6507,42 @@
       });
     }
   };
-})();undefined
+})();
+  Templates["DomElementVariable"] = (function () {
+    return function (parameters, TagManager) {
+      this.get = function () {
+        var selectionMethod = parameters.get("selectionMethod");
+        if (!selectionMethod) {
+          return;
+        }
+        var attributeName = parameters.get("attributeName");
+        var dom = TagManager.dom;
+        var ele;
+        if (selectionMethod === "elementId") {
+          ele = dom.byId(parameters.get("elementId"));
+        } else if (selectionMethod === "cssSelector") {
+          ele = dom.bySelector(parameters.get("cssSelector"));
+          if (ele && ele[0]) {
+            ele = ele[0];
+          } else {
+            ele = null;
+          }
+        }
+        if (ele) {
+          if (attributeName) {
+            if (String(attributeName).toLowerCase() === "value" && ele.nodeName === "INPUT") {
+              var type = dom.getElementAttribute(ele, "type");
+              if (type && type.toLowerCase() === "password") {
+                return;
+              }
+            }
+            return dom.getElementAttribute(ele, attributeName);
+          }
+          return TagManager.dom.getElementText(ele);
+        }
+      };
+    };
+  })();
 
       
       Templates["ClickClassesVariable"] = (function () {
