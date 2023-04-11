638fe3eb4a72848aa5b615bd43cd38435215b23d
nidhishmehra <118984671+nidhishmehra@users.noreply.github.com>
2023-04-11 14:08:01 +0530

    Update mastercontainer.js

--- 6000165802547722822/29d41bcab55e4aa08a1995738f865539/mastercontainer.js
+++ 6000165802547722822/29d41bcab55e4aa08a1995738f865539/mastercontainer.js
@@ -3,7 +3,7 @@
     var d = document,
     s = d.getElementsByTagName('script')[0];
     containerList.forEach(container => {
-      fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/burhan/29d41bcab55e4aa08a1995738f865539/${container}.js`)
+      fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/6000165802547722822/29d41bcab55e4aa08a1995738f865539/${container}.js`)
       .then(response => response.text())
       .then(scriptText => {
         const script = document.createElement('script');
