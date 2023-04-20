0c2e0c5f82d0c4e3068527da3dc5882c4ba754ec
Sachin Sinha <sachin@iqlect.com>
2023-04-20 17:00:19 +0530

    Update file

--- 6000165802547722822/55128542c86647af9c74c596bb22a2c7/35ae1102fc484ce8b2b6cb10cbbea9d2.js
+++ 6000165802547722822/55128542c86647af9c74c596bb22a2c7/35ae1102fc484ce8b2b6cb10cbbea9d2.js
@@ -6676,7 +6676,37 @@
             }, getTriggerInterval());
         };
     };
-})();Templates["DataLayerVariable"] = (function () {
+})();Templates["FullscreenTrigger"] = (function () {
+  return function (parameters, TagManager) {
+    var numTriggers = 0;
+    var events = ["webkitfullscreenchange", "mozfullscreenchange", "fullscreenchange", "MSFullscreenChange"];
+    this.setUp = function (triggerEvent) {
+      function onFullScreen() {
+        var limit = parameters.get("triggerLimit", 1);
+        if (limit) {
+          limit = parseInt(limit, 10);
+        }
+        if (limit && limit <= numTriggers) {
+          return;
+        }
+        var docAlias = parameters.document;
+        var triggerAction = parameters.get("triggerAction", "enter");
+        var isFullscreen = docAlias.fullScreen || docAlias.webkitIsFullScreen || docAlias.mozFullScreen || docAlias.msFullscreenElement;
+        if (isFullscreen && (triggerAction === "any" || triggerAction === "enter")) {
+          triggerEvent({ event: "mtm.Fullscreen", "mtm.fullscreenAction": "enter" });
+          numTriggers++;
+        } else if (!isFullscreen && (triggerAction === "any" || triggerAction === "exit")) {
+          numTriggers++;
+          triggerEvent({ event: "mtm.Fullscreen", "mtm.fullscreenAction": "exit" });
+        }
+      }
+      for (var i = 0; i < events.length; i++) {
+        TagManager.dom.addEventListener(parameters.document, events[i], onFullScreen);
+      }
+    };
+  };
+})();
+Templates["DataLayerVariable"] = (function () {
     return function (parameters, TagManager) {
       this.get = function () {
         var dataLayerName = parameters.get("dataLayerName");
@@ -8167,6 +8197,85 @@
   "Type": "BangDB Analytics",
   "blockedTriggerIds": []
 },
+        {
+  "id": "c225308d-ef8e-4c68-b5ca-f4a09db5ce75",
+  "type": "Matomo",
+  "name": "fs",
+  "parameters": {
+    "matomoConfig": {
+      "name": "Matomo Configuration",
+      "type": "MatomoConfiguration",
+      "lookUpTable": [],
+      "defaultValue": "",
+      "parameters": {
+        "matomoUrl": "https://testbe.bangdb.com:18080",
+        "idSite": "test",
+        "enableLinkTracking": true,
+        "enableCrossDomainLinking": true,
+        "enableDoNotTrack": false,
+        "enableJSErrorTracking": true,
+        "enableHeartBeatTimer": true,
+        "trackAllContentImpressions": true,
+        "trackVisibleContentImpressions": true,
+        "disableCookies": false,
+        "requireConsent": false,
+        "requireCookieConsent": false,
+        "customCookieTimeOutEnable": false,
+        "customCookieTimeOut": 393,
+        "setSecureCookie": true,
+        "cookieDomain": "",
+        "cookiePath": "",
+        "cookieSameSite": "Lax",
+        "disableBrowserFeatureDetection": false,
+        "domains": [],
+        "alwaysUseSendBeacon": false,
+        "userId": "",
+        "customDimensions": [],
+        "bundleTracker": true,
+        "registerAsDefaultTracker": true,
+        "jsEndpoint": "matomo.js",
+        "trackingEndpoint": "stream/ShopIQ/Data"
+      },
+      "Variable": "MatomoConfigurationVariable"
+    },
+    "trackingType": "event",
+    "idGoal": "",
+    "goalCustomRevenue": "",
+    "documentTitle": "",
+    "customUrl": "",
+    "eventCategory": "fs",
+    "eventAction": "fs",
+    "eventName": "fs",
+    "eventValue": {
+      "joinedVariable": [
+        {
+          "Name": "Local Time",
+          "name": "LocalTime",
+          "type": "LocalTime",
+          "lookUpTable": [],
+          "defaultValue": null,
+          "parameters": [],
+          "Variable": "LocalTimeVariable"
+        }
+      ]
+    },
+    "selectedTag": "BangDB Analytics",
+    "Name": "fs",
+    "Description": "fs"
+  },
+  "blockTriggerIds": [],
+  "fireTriggerIds": [
+    "0f1f4d36-3261-4ac9-bd83-89421c043f18"
+  ],
+  "fireLimit": "unlimited",
+  "fireDelay": 0,
+  "startDate": null,
+  "endDate": null,
+  "Tag": "MatomoTag",
+  "idSite": "test",
+  "Type": "BangDB Analytics",
+  "blockedTriggerIds": []
+},
           ],
           triggers: [
             
@@ -8262,6 +8371,20 @@
   "conditions": [],
   "Name": "t5",
   "Description": "t"
+},
+          {
+  "id": "0f1f4d36-3261-4ac9-bd83-89421c043f18",
+  "type": "Fullscreen",
+  "name": "Fullscreen",
+  "Trigger": "FullscreenTrigger",
+  "selectedTrigger": "Full screen",
+  "parameters": {
+    "triggerAction": "any",
+    "triggerLimit": "0"
+  },
+  "conditions": [],
+  "Name": "fs",
+  "Description": "fs"
 },
           ],
           variables: [
