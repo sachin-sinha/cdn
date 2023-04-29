30453e38a08af96dc8bc706592d32e29da73a8c0
Sachin Sinha <sachin@iqlect.com>
2023-04-29 11:18:29 +0530

    Update file

--- 6000165802547722822/6c7749460fb4477faa328540e1f06c5f/5a524fbf535f4e98bfb8ef51532936c7.js
+++ 6000165802547722822/6c7749460fb4477faa328540e1f06c5f/5a524fbf535f4e98bfb8ef51532936c7.js
@@ -6943,7 +6943,218 @@
       };
   };
 })();
-      
+      Templates["CustomHtmlTag"] = (function () {
+    /*!
+     * secure-filters from https://github.com/salesforce/secure-filters/blob/master/lib/secure-filters.js
+     * license: BSD-3-Clause https://github.com/salesforce/secure-filters/blob/master/LICENSE.txt
+     * */
+    function convertControlCharacters(str) {
+        return String(str).replace(/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F]/g, " ");
+    }
+    var secureFilters = {};
+    secureFilters.css = function (val) {
+        var str = String(val);
+        str = convertControlCharacters(str);
+        return str.replace(/[^a-zA-Z0-9\uD800-\uDFFF]/g, function (match) {
+            var code = match.charCodeAt(0);
+            if (code === 0) {
+                return "\\fffd ";
+            } else {
+                var hex = code.toString(16).toLowerCase();
+                return "\\" + hex + " ";
+            }
+        });
+    };
+    secureFilters.html = function (val) {
+        var str = String(val);
+        str = convertControlCharacters(str);
+        return str.replace(/[^\t\n\v\f\r ,\.0-9A-Z_a-z\-\u00A0-\uFFFF]/g, function (match) {
+            var code = match.charCodeAt(0);
+            switch (code) {
+                case 0x22:
+                    return "&quot;";
+                case 0x26:
+                    return "&amp;";
+                case 0x3c:
+                    return "&lt;";
+                case 0x3e:
+                    return "&gt;";
+                default:
+                    if (code < 100) {
+                        var dec = code.toString(10);
+                        return "&#" + dec + ";";
+                    } else {
+                        var hex = code.toString(16).toUpperCase();
+                        return "&#x" + hex + ";";
+                    }
+            }
+        });
+    };
+    secureFilters.style = function (val) {
+        return secureFilters.html(secureFilters.css(val));
+    };
+    secureFilters.uri = function (val) {
+        var QUOT = /\x22/g;
+        var APOS = /\x27/g;
+        var AST = /\*/g;
+        var TILDE = /~/g;
+        var BANG = /!/g;
+        var LPAREN = /\(/g;
+        var RPAREN = /\)/g;
+        var encode = encodeURI(String(val));
+        return encode.replace(BANG, "%21").replace(QUOT, "%27").replace(APOS, "%27").replace(LPAREN, "%28").replace(RPAREN, "%29").replace(AST, "%2A").replace(TILDE, "%7E");
+    }; /*! end secure filters */
+    return function (parameters, TagManager) {
+        function moveChildrenToArray(element) {
+            var children = [];
+            var j = 0;
+            while (j in element.childNodes && element.childNodes.length) {
+                children.push(element.removeChild(element.childNodes[j]));
+            }
+            return children;
+        }
+        function cloneScript(element) {
+            var newScript = parameters.document.createElement("script");
+            var src = TagManager.dom.getElementAttribute(element, "src");
+            if (src) {
+                newScript.setAttribute("src", src);
+            } else {
+                newScript.text = element.text || element.textContent || element.innerHTML || "";
+            }
+            if (element.hasAttribute("id")) {
+                newScript.setAttribute("id", element.getAttribute("id"));
+            }
+            if (element.hasAttribute("charset")) {
+                newScript.setAttribute("charset", element.getAttribute("charset"));
+            }
+            if (element.hasAttribute("defer")) {
+                newScript.setAttribute("defer", element.getAttribute("defer"));
+            }
+            if (element.hasAttribute("async")) {
+                newScript.setAttribute("async", element.getAttribute("async"));
+            }
+            if (element.hasAttribute("onload")) {
+                newScript.setAttribute("onload", element.getAttribute("onload"));
+            }
+            if (element.hasAttribute("type")) {
+                newScript.setAttribute("type", element.getAttribute("type"));
+            }
+            var scriptWithNonce = TagManager.dom.bySelector("script[nonce]");
+            if (scriptWithNonce.length && scriptWithNonce[0]) {
+                scriptWithNonce = scriptWithNonce[0];
+                var nonceAttr = TagManager.dom.getElementAttribute(scriptWithNonce, "nonce");
+                nonceAttr = nonceAttr ? nonceAttr : scriptWithNonce.nonce;
+                newScript.setAttribute("nonce", nonceAttr);
+            }
+            return newScript;
+        }
+        function isJavaScriptElement(element) {
+            if (element && element.nodeName && element.nodeName.toLowerCase() === "script") {
+                var type = TagManager.dom.getElementAttribute(element, "type");
+                if (!type || String(type).toLowerCase() === "text/javascript") {
+                    return true;
+                }
+            }
+            return false;
+        }
+        function doChildrenContainJavaScript(element) {
+            return element && element.innerHTML && element.innerHTML.toLowerCase().indexOf("<script") !== -1;
+        }
+        function insertNode(parent, child, append, previousElement) {
+            if (append || !parent.firstChild) {
+                return parent.appendChild(child);
+            } else {
+                if (previousElement) {
+                    return previousElement.parentNode.insertBefore(child, previousElement.nextSibling);
+                }
+                return parent.insertBefore(child, parent.firstChild);
+            }
+        }
+        function moveNodes(parent, children, append) {
+            var limit = 5000;
+            var counter = 0;
+            var child;
+            var previousElement = null;
+            while (counter in children && children[counter] && counter < limit) {
+                child = children[counter];
+                counter++;
+                if (isJavaScriptElement(child)) {
+                    previousElement = insertNode(parent, cloneScript(child), append, previousElement);
+                } else if (doChildrenContainJavaScript(child)) {
+                    var subChildren = moveChildrenToArray(child);
+                    previousElement = insertNode(parent, child, append, previousElement);
+                    moveNodes(child, subChildren);
+                } else {
+                    previousElement = insertNode(parent, child, append, previousElement);
+                }
+            }
+        }
+        this.fire = function () {
+            var html = parameters.customHtml;
+            if (html && html.type === "JoinedVariable") {
+                var variables = html.getDefinition();
+                var value = "",
+                    varReturn,
+                    theVarValue,
+                    isVariable,
+                    hasValueSet;
+                for (var i = 0; i < variables.length; i++) {
+                    varReturn = parameters.buildVariable(variables[i]);
+                    isVariable = TagManager.utils.isObject(variables[i]);
+                    theVarValue = varReturn.get();
+                    hasValueSet = theVarValue !== false && theVarValue !== null && TagManager.utils.isDefined(theVarValue);
+                    if (isVariable) {
+                        if (TagManager.dom.isElementContext(value, "script")) {
+                            if (!TagManager.utils.isDefined(TagManager.customHtmlDataStore)) {
+                                TagManager.customHtmlDataStore = [];
+                            }
+                            TagManager.customHtmlDataStore.push(theVarValue);
+                            value += "window.MatomoTagManager.customHtmlDataStore[" + (TagManager.customHtmlDataStore.length - 1) + "]";
+                        } else if (TagManager.dom.isElementContext(value, "style") || TagManager.dom.isAttributeContext(value, "style")) {
+                            if (hasValueSet) {
+                                value += secureFilters.css(theVarValue);
+                            } else {
+                                value += "mTmKpwoqM";
+                            }
+                        } else if (TagManager.dom.isAttributeContext(value, "href") || TagManager.dom.isAttributeContext(value, "src")) {
+                            if (hasValueSet) {
+                                value += secureFilters.uri(theVarValue);
+                            }
+                        } else if (hasValueSet) {
+                            value += secureFilters.html(theVarValue);
+                        }
+                    } else if (hasValueSet) {
+                        value += theVarValue;
+                    }
+                }
+                html = value;
+            } else {
+                html = html.get();
+            }
+            if (html) {
+                var div = parameters.document.createElement("div");
+                div.innerHTML = html;
+                if (div.childNodes) {
+                    var children = moveChildrenToArray(div);
+                    var htmlPosition = parameters.get("htmlPosition", "bodyEnd");
+                    var append = true;
+                    if (htmlPosition === "headStart" || htmlPosition === "bodyStart") {
+                        append = false;
+                    }
+                    if (htmlPosition === "headStart" || htmlPosition === "headEnd") {
+                        moveNodes(parameters.document.head, children, append);
+                    } else if (parameters.document.body) {
+                        moveNodes(parameters.document.body, children, append);
+                    } else {
+                        TagManager.dom.onReady(function () {
+                            moveNodes(parameters.document.body, children, append);
+                        });
+                    }
+                }
+            }
+        };
+    };
+})();
       window.MatomoTagManager.addContainer(
         {
           id: "tfVMgnzD",
@@ -6953,6 +7164,29 @@
           environment: "live",
           tags: [
             
+        {
+  "name": "html",
+  "idSite": "s",
+  "type": "CustomHtml",
+  "parameters": {
+    "selectedTag": "Custom HTML",
+    "Name": "html",
+    "Description": "html",
+    "customHTML": "<script>alert('hi'); console.log('hi');</script>",
+    "htmlPosition": "headEnd",
+    "eventValue": {
+      "joinedVariable": [
+        null
+      ]
+    }
+  },
+  "fireTriggerIds": [
+    "605e80a6-fd08-4a79-9c5e-23ac2da4553c"
+  ],
+  "blockedTriggerIds": [],
+  "id": "6fe481a4-5fd0-4eaa-97b9-f0d7093193aa",
+  "Tag": "CustomHtmlTag"
+},
           ],
           triggers: [
             
