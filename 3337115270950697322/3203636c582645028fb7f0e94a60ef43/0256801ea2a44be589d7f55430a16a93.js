cf1fc0613c38fc32c0731ba0daa2688edfc15ab4
Sachin Sinha <sachin@iqlect.com>
2023-05-24 12:28:54 +0530

    Update file

--- 3337115270950697322/3203636c582645028fb7f0e94a60ef43/0256801ea2a44be589d7f55430a16a93.js
+++ 3337115270950697322/3203636c582645028fb7f0e94a60ef43/0256801ea2a44be589d7f55430a16a93.js
@@ -8175,6 +8175,84 @@
   "Type": "BangDB Analytics",
   "blockedTriggerIds": []
 },
+        {
+  "id": "4b444e11-f879-4de3-aafe-a8dcf48cd671",
+  "type": "Matomo",
+  "name": "testEvent2",
+  "parameters": {
+    "matomoConfig": {
+      "name": "Matomo Configuration",
+      "type": "MatomoConfiguration",
+      "lookUpTable": [],
+      "defaultValue": "",
+      "parameters": {
+        "matomoUrl": "https://javeed.bangdb.com:18080",
+        "idSite": "autoparts-miles",
+        "enableLinkTracking": true,
+        "enableCrossDomainLinking": true,
+        "enableDoNotTrack": false,
+        "enableJSErrorTracking": true,
+        "enableHeartBeatTimer": true,
+        "trackAllContentImpressions": true,
+        "trackVisibleContentImpressions": true,
+        "disableCookies": false,
+        "requireConsent": false,
+        "requireCookieConsent": false,
+        "customCookieTimeOutEnable": false,
+        "customCookieTimeOut": 393,
+        "setSecureCookie": true,
+        "cookieDomain": "",
+        "cookiePath": "",
+        "cookieSameSite": "Lax",
+        "disableBrowserFeatureDetection": false,
+        "domains": [],
+        "alwaysUseSendBeacon": false,
+        "userId": "",
+        "customDimensions": [],
+        "bundleTracker": true,
+        "registerAsDefaultTracker": true,
+        "jsEndpoint": "matomo.js",
+        "trackingEndpoint": "stream/Autoparts_miles/Data"
+      },
+      "Variable": "MatomoConfigurationVariable"
+    },
+    "trackingType": "event",
+    "idGoal": "",
+    "goalCustomRevenue": "",
+    "documentTitle": "",
+    "customUrl": "",
+    "eventCategory": "testEvent2",
+    "eventAction": "testEvent2",
+    "eventName": "testEvent2",
+    "eventValue": {
+      "joinedVariable": [
+        {
+          "Name": "Click Classes",
+          "name": "ClickClasses",
+          "type": "ClickClasses",
+          "lookUpTable": [],
+          "defaultValue": null,
+          "parameters": [],
+          "Variable": "ClickClassesVariable"
+        }
+      ]
+    },
+    "selectedTag": "BangDB Analytics",
+    "Name": "testEvent2"
+  },
+  "blockTriggerIds": [],
+  "fireTriggerIds": [
+    "7c3a6f15-41bc-4b5f-b5d3-9063d84a97c4"
+  ],
+  "fireLimit": "unlimited",
+  "fireDelay": 0,
+  "startDate": null,
+  "endDate": null,
+  "Tag": "MatomoTag",
+  "idSite": "autoparts-miles",
+  "Type": "BangDB Analytics",
+  "blockedTriggerIds": []
+},
           ],
           triggers: [
             
