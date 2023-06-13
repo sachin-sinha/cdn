02cb3f5765de950de32f7c44e9b16f41dbc36274
Sachin Sinha <sachin@iqlect.com>
2023-06-13 20:07:12 +0530

    Update file

--- 6000165802547722822/e4c9d47441e64989b1fa9a7041768b2e/182f54edb81f40d1977bb939bbcd92a7.js
+++ 6000165802547722822/e4c9d47441e64989b1fa9a7041768b2e/182f54edb81f40d1977bb939bbcd92a7.js
@@ -8761,90 +8761,6 @@
   "blockedTriggerIds": []
 },
         {
-  "id": "5a61c83b-3bc4-46ff-a3e3-f067a1d633a7",
-  "type": "Matomo",
-  "name": "FormSubmit",
-  "parameters": {
-    "matomoConfig": {
-      "name": "Matomo Configuration",
-      "type": "MatomoConfiguration",
-      "lookUpTable": [],
-      "defaultValue": "",
-      "parameters": {
-        "matomoUrl": "https://testbe.bangdb.com:18080",
-        "idSite": "BangDB : Real-time data platform",
-        "enableLinkTracking": true,
-        "enableCrossDomainLinking": true,
-        "enableDoNotTrack": false,
-        "enableJSErrorTracking": true,
-        "enableHeartBeatTimer": true,
-        "trackAllContentImpressions": true,
-        "trackVisibleContentImpressions": true,
-        "disableCookies": false,
-        "requireConsent": false,
-        "requireCookieConsent": false,
-        "customCookieTimeOutEnable": false,
-        "customCookieTimeOut": 393,
-        "setSecureCookie": true,
-        "cookieDomain": "",
-        "cookiePath": "",
-        "cookieSameSite": "Lax",
-        "disableBrowserFeatureDetection": false,
-        "domains": [],
-        "alwaysUseSendBeacon": false,
-        "userId": "",
-        "customDimensions": [],
-        "bundleTracker": true,
-        "registerAsDefaultTracker": true,
-        "jsEndpoint": "matomo.js",
-        "trackingEndpoint": "stream/Bangdb_clickstream/Data"
-      },
-      "Variable": "MatomoConfigurationVariable"
-    },
-    "trackingType": "event",
-    "idGoal": "",
-    "goalCustomRevenue": "",
-    "documentTitle": "",
-    "customUrl": "",
-    "eventCategory": "FormSubmit",
-    "eventAction": "FormSubmit",
-    "eventName": "FormSubmit",
-    "eventValue": {
-      "joinedVariable": [
-        {
-          "Name": "Form Classes",
-          "name": "FormClasses",
-          "type": "FormClasses",
-          "lookUpTable": [],
-          "defaultValue": null,
-          "parameters": [],
-          "Variable": "FormClassesVariable"
-        }
-      ]
-    },
-    "selectedTag": "BangDB Analytics",
-    "Name": "FormSubmit",
-    "Description": "This tag returns name of the form, when a form is submitted.",
-    "id": "5a61c83b-3bc4-46ff-a3e3-f067a1d633a7",
-    "fireTriggerIds": [
-      "4bf26ce8-8cfb-4793-8add-36c4eecd40b8"
-    ],
-    "blockedTriggerIds": []
-  },
-  "blockTriggerIds": [],
-  "fireTriggerIds": [
-    "4bf26ce8-8cfb-4793-8add-36c4eecd40b8"
-  ],
-  "fireLimit": "unlimited",
-  "fireDelay": 0,
-  "startDate": null,
-  "endDate": null,
-  "Tag": "MatomoTag",
-  "idSite": "BangDB : Real-time data platform",
-  "Type": "BangDB Analytics",
-  "blockedTriggerIds": []
-},
-        {
   "id": "14065f15-cb10-4672-ab0c-a188e92e5b99",
   "type": "Matomo",
   "name": "Download ubuntu16",
@@ -10924,6 +10840,99 @@
   "blockTriggerIds": [],
   "fireTriggerIds": [
     "113611c8-b984-4096-b648-5fe6b86d5549"
+  ],
+  "fireLimit": "unlimited",
+  "fireDelay": 0,
+  "startDate": null,
+  "endDate": null,
+  "Tag": "MatomoTag",
+  "idSite": "BangDB : Real-time data platform",
+  "Type": "BangDB Analytics",
+  "blockedTriggerIds": []
+},
+        {
+  "id": "5a61c83b-3bc4-46ff-a3e3-f067a1d633a7",
+  "type": "Matomo",
+  "name": "FormSubmit",
+  "parameters": {
+    "matomoConfig": {
+      "name": "Matomo Configuration",
+      "type": "MatomoConfiguration",
+      "lookUpTable": [],
+      "defaultValue": "",
+      "parameters": {
+        "matomoUrl": "https://testbe.bangdb.com:18080",
+        "idSite": "BangDB : Real-time data platform",
+        "enableLinkTracking": true,
+        "enableCrossDomainLinking": true,
+        "enableDoNotTrack": false,
+        "enableJSErrorTracking": true,
+        "enableHeartBeatTimer": true,
+        "trackAllContentImpressions": true,
+        "trackVisibleContentImpressions": true,
+        "disableCookies": false,
+        "requireConsent": false,
+        "requireCookieConsent": false,
+        "customCookieTimeOutEnable": false,
+        "customCookieTimeOut": 393,
+        "setSecureCookie": true,
+        "cookieDomain": "",
+        "cookiePath": "",
+        "cookieSameSite": "Lax",
+        "disableBrowserFeatureDetection": false,
+        "domains": [],
+        "alwaysUseSendBeacon": false,
+        "userId": "",
+        "customDimensions": [],
+        "bundleTracker": true,
+        "registerAsDefaultTracker": true,
+        "jsEndpoint": "matomo.js",
+        "trackingEndpoint": "stream/Bangdb_clickstream/Data"
+      },
+      "Variable": "MatomoConfigurationVariable"
+    },
+    "trackingType": "event",
+    "idGoal": "",
+    "goalCustomRevenue": "",
+    "documentTitle": "",
+    "customUrl": "",
+    "eventCategory": "FormSubmit",
+    "eventAction": "FormSubmit",
+    "eventName": "FormSubmit",
+    "eventValue": {
+      "joinedVariable": [
+        {
+          "Name": "Form Classes",
+          "name": "FormClasses",
+          "type": "FormClasses",
+          "lookUpTable": [],
+          "defaultValue": null,
+          "parameters": [],
+          "Variable": "FormClassesVariable"
+        },
+        {
+          "Name": "Form ID",
+          "name": "FormId",
+          "type": "FormId",
+          "lookUpTable": [],
+          "defaultValue": null,
+          "parameters": [],
+          "Variable": "FormIdVariable"
+        }
+      ]
+    },
+    "selectedTag": "BangDB Analytics",
+    "Name": "FormSubmit",
+    "Description": "This tag returns name of the form, when a form is submitted.",
+    "id": "5a61c83b-3bc4-46ff-a3e3-f067a1d633a7",
+    "fireTriggerIds": [
+      "4bf26ce8-8cfb-4793-8add-36c4eecd40b8"
+    ],
+    "blockedTriggerIds": []
+  },
+  "blockTriggerIds": [],
+  "fireTriggerIds": [
+    "4bf26ce8-8cfb-4793-8add-36c4eecd40b8"
   ],
   "fireLimit": "unlimited",
   "fireDelay": 0,
