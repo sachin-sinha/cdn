d4db97b0608fb33c3f52e22658d47f60df3297bd
Sachin Sinha <sachin@iqlect.com>
2023-06-16 11:47:04 +0530

    Update file

--- 3337115270950697322/97071fd8ec944a18866ae7810e4ffb6f/fed95702dbdc4f81ad0d54a061260957.js
+++ 3337115270950697322/97071fd8ec944a18866ae7810e4ffb6f/fed95702dbdc4f81ad0d54a061260957.js
@@ -6732,6 +6732,72 @@
       });
     };
   };
+})();Templates["FormSubmitTrigger"] = (function () {
+  return function (parameters, TagManager) {
+    this.setUp = function (triggerEvent) {
+      TagManager.dom.onReady(function () {
+        TagManager.dom.addEventListener(
+          parameters.document.body,
+          "submit",
+          function (event) {
+            if (!event.target) {
+              return;
+            }
+            var target = event.target;
+            if (target.nodeName === "FORM") {
+              var dom = TagManager.dom;
+              var formAction = dom.getElementAttribute(target, "action");
+              if (!formAction) {
+                formAction = parameters.window.location.href;
+              }
+              triggerEvent({
+                event: "mtm.FormSubmit",
+                "mtm.formElement": target,
+                "mtm.formElementId": dom.getElementAttribute(target, "id"),
+                "mtm.formElementName": dom.getElementAttribute(target, "name"),
+                "mtm.formElementClasses": dom.getElementClassNames(target),
+                "mtm.formElementAction": formAction,
+              });
+            }
+          },
+          true
+        );
+      });
+    };
+  };
+})();Templates["FormSubmitTrigger"] = (function () {
+  return function (parameters, TagManager) {
+    this.setUp = function (triggerEvent) {
+      TagManager.dom.onReady(function () {
+        TagManager.dom.addEventListener(
+          parameters.document.body,
+          "submit",
+          function (event) {
+            if (!event.target) {
+              return;
+            }
+            var target = event.target;
+            if (target.nodeName === "FORM") {
+              var dom = TagManager.dom;
+              var formAction = dom.getElementAttribute(target, "action");
+              if (!formAction) {
+                formAction = parameters.window.location.href;
+              }
+              triggerEvent({
+                event: "mtm.FormSubmit",
+                "mtm.formElement": target,
+                "mtm.formElementId": dom.getElementAttribute(target, "id"),
+                "mtm.formElementName": dom.getElementAttribute(target, "name"),
+                "mtm.formElementClasses": dom.getElementClassNames(target),
+                "mtm.formElementAction": formAction,
+              });
+            }
+          },
+          true
+        );
+      });
+    };
+  };
 })();Templates["ReferrerUrlVariable"] = (function () {
     return function (parameters, TagManager) {
       this.get = function () {
@@ -8096,14 +8162,17 @@
     "Name": "CallButton",
     "id": "90ae6cd5-2abc-41ea-8d58-703b81941fc7",
     "fireTriggerIds": [
-      "c2b9d594-01fb-434a-99fc-e7ecb4fec163"
+      "c2b9d594-01fb-434a-99fc-e7ecb4fec163",
+      "129d0dfb-43f4-4f10-a1ee-489f4aa5b0ed"
     ],
     "blockedTriggerIds": []
   },
   "blockTriggerIds": [],
   "fireTriggerIds": [
     "c2b9d594-01fb-434a-99fc-e7ecb4fec163",
-    "129d0dfb-43f4-4f10-a1ee-489f4aa5b0ed"
+    "129d0dfb-43f4-4f10-a1ee-489f4aa5b0ed",
+    "712f73ac-8e05-4207-8fbd-2bf482a0b5ec",
+    "c8854057-315f-4c1d-a896-840e62ae3a43"
   ],
   "fireLimit": "unlimited",
   "fireDelay": 0,
@@ -8266,11 +8335,59 @@
         "parameters": [],
         "Variable": "FormClassesVariable"
       },
-      "comparison": "contains",
-      "expected": "BDBCallTracker"
+      "comparison": "equals",
+      "expected": "tel BDBCallTracker"
     }
   ],
   "Name": "CallButton"
+},
+            {
+  "id": "712f73ac-8e05-4207-8fbd-2bf482a0b5ec",
+  "type": "FormSubmit",
+  "name": "FormSubmit",
+  "Trigger": "FormSubmitTrigger",
+  "selectedTrigger": "Form Submit",
+  "parameters": {},
+  "conditions": [
+    {
+      "actual": {
+        "Name": "Form Classes",
+        "name": "FormClasses",
+        "type": "FormClasses",
+        "lookUpTable": [],
+        "defaultValue": null,
+        "parameters": [],
+        "Variable": "FormClassesVariable"
+      },
+      "comparison": "equals",
+      "expected": "slide-btn BDBCallTracker"
+    }
+  ],
+  "Name": "CallButton2"
+},
+            {
+  "id": "c8854057-315f-4c1d-a896-840e62ae3a43",
+  "type": "FormSubmit",
+  "name": "FormSubmit",
+  "Trigger": "FormSubmitTrigger",
+  "selectedTrigger": "Form Submit",
+  "parameters": {},
+  "conditions": [
+    {
+      "actual": {
+        "Name": "Form Classes",
+        "name": "FormClasses",
+        "type": "FormClasses",
+        "lookUpTable": [],
+        "defaultValue": null,
+        "parameters": [],
+        "Variable": "FormClassesVariable"
+      },
+      "comparison": "equals",
+      "expected": "BDBCallTracker"
+    }
+  ],
+  "Name": "CallButton3"
 },
           ],
           variables: [
