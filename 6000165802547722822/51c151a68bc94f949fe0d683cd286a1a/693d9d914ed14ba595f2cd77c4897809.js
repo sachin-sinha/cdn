1cacd427ec5a2729a62924abafe2a0cf4d37dfef
Sachin Sinha <sachin@iqlect.com>
2023-07-08 15:22:26 +0530

    Update file

--- 6000165802547722822/51c151a68bc94f949fe0d683cd286a1a/693d9d914ed14ba595f2cd77c4897809.js
+++ 6000165802547722822/51c151a68bc94f949fe0d683cd286a1a/693d9d914ed14ba595f2cd77c4897809.js
@@ -7249,94 +7249,6 @@
           tags: [
             
         {
-  "id": "8089dfff-1b95-4689-8be9-31a9bf2a7671",
-  "type": "Matomo",
-  "name": "product_view",
-  "parameters": {
-    "matomoConfig": {
-      "name": "Matomo Configuration",
-      "type": "MatomoConfiguration",
-      "lookUpTable": [],
-      "defaultValue": "",
-      "parameters": {
-        "matomoUrl": "https://testbe.bangdb.com:18080",
-        "idSite": "Ecomm - test website",
-        "enableLinkTracking": true,
-        "enableCrossDomainLinking": true,
-        "enableDoNotTrack": false,
-        "enableJSErrorTracking": true,
-        "enableHeartBeatTimer": true,
-        "trackAllContentImpressions": true,
-        "trackVisibleContentImpressions": true,
-        "disableCookies": false,
-        "requireConsent": false,
-        "requireCookieConsent": false,
-        "customCookieTimeOutEnable": false,
-        "customCookieTimeOut": 393,
-        "setSecureCookie": true,
-        "cookieDomain": "",
-        "cookiePath": "",
-        "cookieSameSite": "Lax",
-        "disableBrowserFeatureDetection": false,
-        "domains": [],
-        "alwaysUseSendBeacon": false,
-        "userId": "",
-        "customDimensions": [],
-        "bundleTracker": true,
-        "registerAsDefaultTracker": true,
-        "jsEndpoint": "matomo.js",
-        "trackingEndpoint": "stream/ecomm_clickstream/Data"
-      },
-      "Variable": "MatomoConfigurationVariable"
-    },
-    "trackingType": "event",
-    "idGoal": "",
-    "goalCustomRevenue": "",
-    "documentTitle": "",
-    "customUrl": "",
-    "eventCategory": "product-info",
-    "eventAction": "product-info",
-    "eventName": "product_view",
-    "eventValue": {
-      "joinedVariable": [
-        {
-          "selectedVariable": "Custom JavaScript",
-          "name": "CustomJsFunction",
-          "type": "CustomJsFunction",
-          "Name": "product_view",
-          "Description": "Product viewed",
-          "jsFunction": "function() {const elements = document.querySelectorAll('[id^=\"bdb-tracker\"]');const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}",
-          "id": "70a5f886-fc74-4985-aaaf-ea10d3a2a4fa",
-          "parameters": {
-            "selectedVariable": "Custom JavaScript",
-            "name": "CustomJsFunction",
-            "type": "CustomJsFunction",
-            "Name": "product_view",
-            "Description": "Product viewed",
-            "jsFunction": "function() {const elements = document.querySelectorAll('[id^=\"bdb-tracker\"]');const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}"
-          },
-          "Variable": "CustomJsFunctionVariable70a5f886fc744985aaafea10d3a2a4fa"
-        }
-      ]
-    },
-    "selectedTag": "BangDB Analytics",
-    "Name": "product_view",
-    "Description": "Product viewed"
-  },
-  "blockTriggerIds": [],
-  "fireTriggerIds": [
-    "9b3b79e4-3f58-4649-85d6-3e51459b9b54"
-  ],
-  "fireLimit": "unlimited",
-  "fireDelay": 0,
-  "startDate": null,
-  "endDate": null,
-  "Tag": "MatomoTag",
-  "idSite": "Ecomm - test website",
-  "Type": "BangDB Analytics",
-  "blockedTriggerIds": []
-},
-        {
   "id": "9317f9df-3d8a-4aac-9a68-4bc9d02b2b78",
   "type": "Matomo",
   "name": "Location",
@@ -7445,6 +7357,144 @@
   "blockTriggerIds": [],
   "fireTriggerIds": [
     "e24eb80b-6070-4d67-a4b0-16ea95289ec1"
+  ],
+  "fireLimit": "unlimited",
+  "fireDelay": 0,
+  "startDate": null,
+  "endDate": null,
+  "Tag": "MatomoTag",
+  "idSite": "Ecomm - test website",
+  "Type": "BangDB Analytics",
+  "blockedTriggerIds": []
+},
+        {
+  "id": "8089dfff-1b95-4689-8be9-31a9bf2a7671",
+  "type": "Matomo",
+  "name": "product_view",
+  "parameters": {
+    "matomoConfig": {
+      "name": "Matomo Configuration",
+      "type": "MatomoConfiguration",
+      "lookUpTable": [],
+      "defaultValue": "",
+      "parameters": {
+        "matomoUrl": "https://testbe.bangdb.com:18080",
+        "idSite": "Ecomm - test website",
+        "enableLinkTracking": true,
+        "enableCrossDomainLinking": true,
+        "enableDoNotTrack": false,
+        "enableJSErrorTracking": true,
+        "enableHeartBeatTimer": true,
+        "trackAllContentImpressions": true,
+        "trackVisibleContentImpressions": true,
+        "disableCookies": false,
+        "requireConsent": false,
+        "requireCookieConsent": false,
+        "customCookieTimeOutEnable": false,
+        "customCookieTimeOut": 393,
+        "setSecureCookie": true,
+        "cookieDomain": "",
+        "cookiePath": "",
+        "cookieSameSite": "Lax",
+        "disableBrowserFeatureDetection": false,
+        "domains": [],
+        "alwaysUseSendBeacon": false,
+        "userId": "",
+        "customDimensions": [],
+        "bundleTracker": true,
+        "registerAsDefaultTracker": true,
+        "jsEndpoint": "matomo.js",
+        "trackingEndpoint": "stream/ecomm_clickstream/Data"
+      },
+      "Variable": "MatomoConfigurationVariable"
+    },
+    "trackingType": "event",
+    "idGoal": "",
+    "goalCustomRevenue": "",
+    "documentTitle": "",
+    "customUrl": "",
+    "eventCategory": "product-info",
+    "eventAction": "product-info",
+    "eventName": "product_view",
+    "eventValue": {
+      "joinedVariable": [
+        {
+          "selectedVariable": "Custom JavaScript",
+          "name": "CustomJsFunction",
+          "type": "CustomJsFunction",
+          "Name": "product_view",
+          "Description": "Product viewed",
+          "jsFunction": "function() {const elements = document.querySelectorAll('[id^=\"bdb-tracker\"]');const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}",
+          "id": "70a5f886-fc74-4985-aaaf-ea10d3a2a4fa",
+          "parameters": {
+            "selectedVariable": "Custom JavaScript",
+            "name": "CustomJsFunction",
+            "type": "CustomJsFunction",
+            "Name": "product_view",
+            "Description": "Product viewed",
+            "jsFunction": "function() {const elements = document.querySelectorAll('[id^=\"bdb-tracker\"]');const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}"
+          },
+          "Variable": "CustomJsFunctionVariable70a5f886fc744985aaafea10d3a2a4fa"
+        },
+        {
+          "selectedVariable": "Custom JavaScript",
+          "name": "CustomJsFunction",
+          "type": "CustomJsFunction",
+          "Name": "product_view",
+          "Description": "Product viewed",
+          "jsFunction": "function() {const elements = document.querySelectorAll(`[id^='bdb-tracker']`);const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}",
+          "id": "70a5f886-fc74-4985-aaaf-ea10d3a2a4fa",
+          "parameters": {
+            "selectedVariable": "Custom JavaScript",
+            "name": "CustomJsFunction",
+            "type": "CustomJsFunction",
+            "Name": "product_view",
+            "Description": "Product viewed",
+            "jsFunction": "function() {const elements = document.querySelectorAll(`[id^='bdb-tracker']`);const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}",
+            "id": "70a5f886-fc74-4985-aaaf-ea10d3a2a4fa",
+            "parameters": {
+              "selectedVariable": "Custom JavaScript",
+              "name": "CustomJsFunction",
+              "type": "CustomJsFunction",
+              "Name": "product_view",
+              "Description": "Product viewed",
+              "jsFunction": "function() {const elements = document.querySelectorAll('[id^='bdb-tracker']');const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}",
+              "id": "70a5f886-fc74-4985-aaaf-ea10d3a2a4fa",
+              "parameters": {
+                "selectedVariable": "Custom JavaScript",
+                "name": "CustomJsFunction",
+                "type": "CustomJsFunction",
+                "Name": "product_view",
+                "Description": "Product viewed",
+                "jsFunction": "function() {const elements = document.querySelectorAll('[id^=\"bdb-tracker\"]');const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}",
+                "id": "70a5f886-fc74-4985-aaaf-ea10d3a2a4fa",
+                "parameters": {
+                  "selectedVariable": "Custom JavaScript",
+                  "name": "CustomJsFunction",
+                  "type": "CustomJsFunction",
+                  "Name": "product_view",
+                  "Description": "Product viewed",
+                  "jsFunction": "function() {const elements = document.querySelectorAll('[id^=\"bdb-tracker\"]');const elementData = {};elements.forEach(element => {element.id.replace('bdb-tracker-', '');const arr = element?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;});return JSON.stringify(elementData)}"
+                }
+              }
+            }
+          },
+          "Variable": "CustomJsFunctionVariable70a5f886fc744985aaafea10d3a2a4fa"
+        }
+      ]
+    },
+    "selectedTag": "BangDB Analytics",
+    "Name": "product_view",
+    "Description": "Product viewed",
+    "id": "8089dfff-1b95-4689-8be9-31a9bf2a7671",
+    "fireTriggerIds": [
+      "9b3b79e4-3f58-4649-85d6-3e51459b9b54"
+    ],
+    "blockedTriggerIds": []
+  },
+  "blockTriggerIds": [],
+  "fireTriggerIds": [
+    "9b3b79e4-3f58-4649-85d6-3e51459b9b54"
   ],
   "fireLimit": "unlimited",
   "fireDelay": 0,
