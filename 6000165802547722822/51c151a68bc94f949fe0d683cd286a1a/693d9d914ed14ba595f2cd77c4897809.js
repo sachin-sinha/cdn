b7ec84bbfebda8d41d31893214ad5ea47edfe163
Sachin Sinha <sachin@iqlect.com>
2023-07-08 16:38:22 +0530

    Update file

--- 6000165802547722822/51c151a68bc94f949fe0d683cd286a1a/693d9d914ed14ba595f2cd77c4897809.js
+++ 6000165802547722822/51c151a68bc94f949fe0d683cd286a1a/693d9d914ed14ba595f2cd77c4897809.js
@@ -6862,7 +6862,7 @@
           
           Templates["CustomJsFunctionVariable9d6ec8556ac544cd96ea1c63678ae2f7"] = (function () {
             return function (parameters, TagManager) {
-                this.get = function() {var fd = {};var fe=document.querySelector('form.sandy-product');fd.price = fe.getElementsByClassName('price-new')[0].innerText;fd.product=fe.elements['product_id'].value;fd.quantity=fe.elements['quantity'].value;return JSON.stringify(fd);}
+                this.get = function() {var fd = {};var fe=document.querySelector('form.sandy-product');console.log('this is what got triggered', fd);fd.product=fe.elements['product_id'].value;fd.quantity=fe.elements['quantity'].value;return JSON.stringify(fd);}
             };
         })()
           
@@ -8021,7 +8021,7 @@
   }
 }, Variable: "DataLayerVariable"},
           { name: "CustomJsFunction", type: "CustomJsFunction", lookUpTable: [], defaultValue: "", parameters: { jsFunction: "function() {const elements = document.querySelectorAll(`[id^='bdb-tracker']`);console.log('new tag got triggered 4');const elementData = {};elements.forEach(element => {const temp = element.id.replace('bdb-tracker-', '');if (temp.includes('-')) {const arr = temp?.split('-') || [];const id = arr[0] || 'NA';const text = arr[1] || 'NA';elementData[id] = text;}});console.log(JSON.stringify(elementData), elementData); return JSON.stringify(elementData);}" }, Variable: "CustomJsFunctionVariable70a5f886fc744985aaafea10d3a2a4fa" },
-          { name: "CustomJsFunction", type: "CustomJsFunction", lookUpTable: [], defaultValue: "", parameters: { jsFunction: "function() {var fd = {};var fe=document.querySelector('form.sandy-product');fd.price = fe.getElementsByClassName('price-new')[0].innerText;fd.product=fe.elements['product_id'].value;fd.quantity=fe.elements['quantity'].value;return JSON.stringify(fd);}" }, Variable: "CustomJsFunctionVariable9d6ec8556ac544cd96ea1c63678ae2f7" },
+          { name: "CustomJsFunction", type: "CustomJsFunction", lookUpTable: [], defaultValue: "", parameters: { jsFunction: "function() {var fd = {};var fe=document.querySelector('form.sandy-product');console.log('this is what got triggered', fd);fd.product=fe.elements['product_id'].value;fd.quantity=fe.elements['quantity'].value;return JSON.stringify(fd);}" }, Variable: "CustomJsFunctionVariable9d6ec8556ac544cd96ea1c63678ae2f7" },
             {
               name: "MatomoConfiguration",
               type: "MatomoConfiguration",
