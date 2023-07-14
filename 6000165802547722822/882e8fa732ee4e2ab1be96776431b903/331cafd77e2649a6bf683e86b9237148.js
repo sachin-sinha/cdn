55cc6161368a60524aecbefb0c09e8a299a18775
Sachin Sinha <sachin@iqlect.com>
2023-07-14 16:28:59 +0530

    Update file

--- 6000165802547722822/882e8fa732ee4e2ab1be96776431b903/331cafd77e2649a6bf683e86b9237148.js
+++ 6000165802547722822/882e8fa732ee4e2ab1be96776431b903/331cafd77e2649a6bf683e86b9237148.js
@@ -6575,6 +6575,90 @@
       triggerEvent({ event: "mtm.PageView" });
     };
   };
+})();Templates["HistoryChangeTrigger"] = (function () {
+  return function (parameters, TagManager) {
+    function getCurrentUrl() {
+      return parameters.window.location.href;
+    }
+    function getEventUrl(event) {
+      if (event && event.target && event.target.location && event.target.location.href) {
+        return event.target.location.href;
+      }
+      return getCurrentUrl();
+    }
+    this.setUp = function (triggerEvent) {
+      var initialUrl = getCurrentUrl();
+      var url = TagManager.url;
+      var origin = url.parseUrl(initialUrl, "origin");
+      var lastEvent = { eventType: null, hash: url.parseUrl(initialUrl, "hash"), search: url.parseUrl(initialUrl, "search"), path: url.parseUrl(initialUrl, "pathname"), state: parameters.window.state || null };
+      function trigger(eventType, newUrl, newState) {
+        var newEvent = { eventType: eventType, hash: url.parseUrl(newUrl, "hash"), search: url.parseUrl(newUrl, "search"), path: url.parseUrl(newUrl, "pathname"), state: newState };
+        var shouldForceEvent =
+          (lastEvent.eventType === "popstate" && newEvent.eventType === "hashchange") ||
+          (lastEvent.eventType === "hashchange" && newEvent.eventType === "popstate") ||
+          (lastEvent.eventType === "hashchange" && newEvent.eventType === "hashchange") ||
+          (lastEvent.eventType === "popstate" && newEvent.eventType === "popstate");
+        shouldForceEvent = !shouldForceEvent;
+        var oldUrl = lastEvent.path;
+        if (lastEvent.search) {
+          oldUrl += "?" + lastEvent.search;
+        }
+        if (lastEvent.hash) {
+          oldUrl += "#" + lastEvent.hash;
+        }
+        var nowUrl = newEvent.path;
+        if (newEvent.search) {
+          nowUrl += "?" + newEvent.search;
+        }
+        if (newEvent.hash) {
+          nowUrl += "#" + newEvent.hash;
+        }
+        if (shouldForceEvent || oldUrl !== nowUrl) {
+          var tmpLast = lastEvent;
+          lastEvent = newEvent;
+          triggerEvent({
+            event: "mtm.HistoryChange",
+            "mtm.historyChangeSource": newEvent.eventType,
+            "mtm.oldUrl": origin + oldUrl,
+            "mtm.newUrl": origin + nowUrl,
+            "mtm.oldUrlHash": tmpLast.hash,
+            "mtm.newUrlHash": newEvent.hash,
+            "mtm.oldUrlPath": tmpLast.path,
+            "mtm.newUrlPath": newEvent.path,
+            "mtm.oldUrlSearch": tmpLast.search,
+            "mtm.newUrlSearch": newEvent.search,
+            "mtm.oldHistoryState": tmpLast.state,
+            "mtm.newHistoryState": newEvent.state,
+          });
+        }
+      }
+      function replaceHistoryMethod(methodNameToReplace) {
+        TagManager.utils.setMethodWrapIfNeeded(parameters.window.history, methodNameToReplace, function (state, title, urlParam) {
+          trigger(methodNameToReplace, getCurrentUrl(), state);
+        });
+      }
+      replaceHistoryMethod("replaceState");
+      replaceHistoryMethod("pushState");
+      TagManager.dom.addEventListener(
+        parameters.window,
+        "hashchange",
+        function (event) {
+          var newUrl = getEventUrl(event);
+          trigger("hashchange", newUrl, null);
+        },
+        false
+      );
+      TagManager.dom.addEventListener(
+        parameters.window,
+        "popstate",
+        function (event) {
+          var newUrl = getEventUrl(event);
+          trigger("popstate", newUrl, event.state);
+        },
+        false
+      );
+    };
+  };
 })();Templates["UrlVariable"] = (function () {
     return function (parameters, TagManager) {
       this.get = function () {
@@ -7686,9 +7770,9 @@
     "goalCustomRevenue": "",
     "documentTitle": "",
     "customUrl": "",
-    "eventCategory": "newPageView",
-    "eventAction": "newPageView",
-    "eventName": "newPageView",
+    "eventCategory": "newPageView2",
+    "eventAction": "newPageView2",
+    "eventName": "newPageView2",
     "eventValue": {
       "joinedVariable": [
         {
@@ -7721,7 +7805,7 @@
   },
   "blockTriggerIds": [],
   "fireTriggerIds": [
-    "54907261-5462-4f05-a917-b34119f625b0"
+    "d7d7bfb3-bb98-45e0-998e-ff754f39c627"
   ],
   "fireLimit": "unlimited",
   "fireDelay": 0,
@@ -7805,6 +7889,17 @@
   "conditions": [],
   "Name": "New page view trigger",
   "Description": "This is the new page view trigger"
+},
+            {
+  "id": "d7d7bfb3-bb98-45e0-998e-ff754f39c627",
+  "type": "HistoryChange",
+  "name": "HistoryChange",
+  "Trigger": "HistoryChangeTrigger",
+  "selectedTrigger": "History Change",
+  "parameters": {},
+  "conditions": [],
+  "Name": "history change trigger",
+  "Description": "this is the history change trigger"
 },
           ],
           variables: [
