b939d52cd32eb26954d2722e581d564d7a0cc894
nidhishmehra <118984671+nidhishmehra@users.noreply.github.com>
2023-07-21 16:39:46 +0530

    CDN migration checkpoint

--- GheC7dJe8Rnv/6a7b47128b3e48b8a246a025fde07979/mastercontainer.js
+++ GheC7dJe8Rnv/6a7b47128b3e48b8a246a025fde07979/mastercontainer.js
@@ -1,15 +1,10 @@
-
-  const containerList = ["636eec234aca4f5882c8d92c89ad2090"];
-  var d = document,
-  s = d.getElementsByTagName('script')[0];
-  containerList.forEach(container => {
-    fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/GheC7dJe8Rnv/6a7b47128b3e48b8a246a025fde07979/${container}.js`)
-    .then(response => response.text())
-    .then(scriptText => {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.async = true;
-      script.text = scriptText;
-      s.parentNode.insertBefore(script, s);
-    });
-  })
+const containerList = ["636eec234aca4f5882c8d92c89ad2090"];
+var d = document,
+s = d.getElementsByTagName('script')[0];
+containerList.forEach(container => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = `https://cdn.jsdelivr.net/gh/sachin-sinha/cdn@master/GheC7dJe8Rnv/6a7b47128b3e48b8a246a025fde07979/${container}.min.js`;
+    s.parentNode.insertBefore(script, s);
+})
