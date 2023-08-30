c1c335198c759d849f5f55c2950a647c0b507a10
nidhishmehra <118984671+nidhishmehra@users.noreply.github.com>
2023-08-30 20:00:36 +0530

    Update mastercontainer.js

--- GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/mastercontainer.js
+++ GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/mastercontainer.js
@@ -1,11 +1,16 @@
-
-  const containerList = ["496ce8f65af94180b1b148723ff3e9c5","964d1b68c54d4f31a9c5ff51221a44a9"];
-  var d = document,
-  s = d.getElementsByTagName('script')[0];
-  containerList.forEach(container => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = `https://cdn.jsdelivr.net/gh/sachin-sinha/cdn@master/GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/${container}.js`;
-    s.parentNode.insertBefore(script, s);
-  })
+const containerList = ["496ce8f65af94180b1b148723ff3e9c5","964d1b68c54d4f31a9c5ff51221a44a9"];
+var d = document,
+s = d.getElementsByTagName('script')[0];
+containerList.forEach(container => {
+  fetch(
+        "https://raw.githubusercontent.com/sachin-sinha/cdn/master/GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/${container}.js"
+      )
+        .then((response) => response.text())
+        .then((scriptText) => {
+          const script = document.createElement("script");
+          script.type = "text/javascript";
+          script.async = true;
+          script.text = scriptText;
+          s.parentNode.insertBefore(script, s);
+        });
+})
