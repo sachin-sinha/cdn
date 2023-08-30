a55245770f241ba4cc2620ff69f5903b81f1ef17
Sachin Sinha <sachin@iqlect.com>
2023-08-30 20:22:48 +0530

    Update file

--- GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/mastercontainer.js
+++ GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/mastercontainer.js
@@ -2,15 +2,14 @@
   const containerList = ["496ce8f65af94180b1b148723ff3e9c5","964d1b68c54d4f31a9c5ff51221a44a9"];
   var d = document,
   s = d.getElementsByTagName('script')[0];
-
-  fetch(
-    `https://raw.githubusercontent.com/sachin-sinha/cdn/master/GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/${container}.js`
-  )
-    .then((response) => response.text())
-    .then((scriptText) => {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
+  containerList.forEach(container => {
+    fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/GheC7dJe8Rnv/dcebba5bfb6b498bbb9cd28ec8b0e2f6/${container}.js`)
+    .then(response => response.text())
+    .then(scriptText => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
       script.async = true;
       script.text = scriptText;
       s.parentNode.insertBefore(script, s);
     });
+  })
