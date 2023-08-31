2824e8d35275bf3716b6682f2002dc9b8647e6e2
Sachin Sinha <sachin@iqlect.com>
2023-08-31 14:30:15 +0530

    Version

---
+++ GheC7dJe8Rnv/0c4cb9b53198435292478055d7a58543/mastercontainer.js
@@ -0,0 +1,15 @@
+
+  const containerList = ["496ce8f65af94180b1b148723ff3e9c5","99295212407b4842ac77401779dd87ac"];
+  var d = document,
+  s = d.getElementsByTagName('script')[0];
+  containerList.forEach(container => {
+    fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/GheC7dJe8Rnv/0c4cb9b53198435292478055d7a58543/${container}.js`)
+    .then(response => response.text())
+    .then(scriptText => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.text = scriptText;
+      s.parentNode.insertBefore(script, s);
+    });
+  })
