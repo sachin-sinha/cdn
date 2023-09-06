d0aaaf4a6604aff9d7e03b7f15055a62c9d296e9
nidhishmehra <118984671+nidhishmehra@users.noreply.github.com>
2023-09-06 13:22:01 +0530

    Update mastercontainer.js

--- 3337115270950697322/09215d884a43488588a098f40bcab188/mastercontainer.js
+++ 3337115270950697322/09215d884a43488588a098f40bcab188/mastercontainer.js
@@ -1,12 +1,15 @@
 
-    const containerList = ["2eed2b95258d4809a1e7e9dea87f4a1b"];
-    var d = document,
+const containerList = ["2eed2b95258d4809a1e7e9dea87f4a1b"];
+var d = document,
     s = d.getElementsByTagName('script')[0];
     containerList.forEach(container => {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.async = true;
-      script.src = `https://cdn.jsdelivr.net/gh/sachin-sinha/cdn@master/3337115270950697322/09215d884a43488588a098f40bcab188/${container}.js`;
-      s.parentNode.insertBefore(script, s);
+      fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/3337115270950697322/09215d884a43488588a098f40bcab188/${container}.js`)
+      .then(response => response.text())
+      .then(scriptText => {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.text = scriptText;
+        s.parentNode.insertBefore(script, s);
+      });
     })
-  
