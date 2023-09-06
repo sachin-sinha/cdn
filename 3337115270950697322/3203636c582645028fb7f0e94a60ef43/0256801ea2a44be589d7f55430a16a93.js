bfe92608bace57548242b4be8caac9c2749b769a
nidhishmehra <118984671+nidhishmehra@users.noreply.github.com>
2023-09-06 12:38:36 +0530

    Update 0256801ea2a44be589d7f55430a16a93.js

--- 3337115270950697322/3203636c582645028fb7f0e94a60ef43/0256801ea2a44be589d7f55430a16a93.js
+++ 3337115270950697322/3203636c582645028fb7f0e94a60ef43/0256801ea2a44be589d7f55430a16a93.js
@@ -8626,8 +8626,8 @@
         "parameters": [],
         "Variable": "ClickDestinationUrlVariable"
       },
-      "comparison": "contains",
-      "expected": "https://"
+      "comparison": "regexp",
+      "expected": "https:\\/\\/|#"
     }
   ],
   "Name": "All Links Clicked"
