c51d5bd872769ce852140c7f3d8ffb8a4f0970c1
Sachin Sinha <sachin@iqlect.com>
2023-09-06 12:37:53 +0530

    Update file

--- KpG4GoZiM6Py/3203636c582645028fb7f0e94a60ef43/mastercontainer.js
+++ KpG4GoZiM6Py/3203636c582645028fb7f0e94a60ef43/mastercontainer.js
@@ -3,9 +3,13 @@
   var d = document,
   s = d.getElementsByTagName('script')[0];
   containerList.forEach(container => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = `https://cdn.jsdelivr.net/gh/sachin-sinha/cdn@master/KpG4GoZiM6Py/3203636c582645028fb7f0e94a60ef43/${container}.js`;
-    s.parentNode.insertBefore(script, s);
+    fetch(`https://raw.githubusercontent.com/sachin-sinha/cdn/master/KpG4GoZiM6Py/3203636c582645028fb7f0e94a60ef43/${container}.js`)
+    .then(response => response.text())
+    .then(scriptText => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.text = scriptText;
+      s.parentNode.insertBefore(script, s);
+    });
   })
